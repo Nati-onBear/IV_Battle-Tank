60dae9bc5a0ba8a31ea147bd82d6446048574893
Nati-onBear <wokhug99@gmail.com>
2019-05-21 22:46:03 +0700

    Using Enum for AimingStates

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"		// Inherited
 #include "TankAimingComponent.generated.h"
 
+// Enum for aim states
+UENUM(BlueprintType)
+enum class EAimingStatus : uint8
+{
+	AE_Locked		UMETA(DisplayName = "Locked"),
+	AE_Aiming		UMETA(DisplayName = "Aiming"),
+	AE_Reloading	UMETA(DisplayName = "Reloading")
+};
+
 // Forward Declaration: let header know that the class exist
 class UTankBarrel;		
 class UTankTurret;
@@ -25,6 +34,10 @@ public:
 
 	void SetTurretReference(UTankTurret*);
 
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = State)
+	EAimingStatus AimStatus = EAimingStatus::AE_Reloading;
+
 private:
 	UTankBarrel* Barrel = nullptr;
 
@@ -32,4 +45,6 @@ private:
 
 	// Method for elevating barrel in PITCH (Y value) and rotating turret in YAW (Z value)
 	void MoveComponent(FVector AimDirection);
+
+	
 };
